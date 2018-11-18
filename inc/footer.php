d949102040d8675df1e3674233f91547deca6a2c
MD. JAHIDUL HOSSAIN <jahidcsensu@gmail.com>
2018-11-18 22:01:46 +0600

    Footer and header file

---
+++ inc/footer.php
@@ -0,0 +1,106 @@
+<!-- Footer -->
+<div class="footer w3ls">
+	<div class="container">
+		<div class="footer-main"><!-- Footer -->
+<div class="footer w3ls">
+	<div class="container">
+		<div class="footer-main">
+			<div class="footer-top">
+				<div class="col-md-4 services-grids">
+					<h3>About Us</h3>
+					<ul>
+						<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><a >Centre of Excellence</a></li>
+						<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><a >Excellence in Surgeries</a></li>
+						<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><a >Pioneer in Emergency Handling</a></li>
+						<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><a >Future Plans</a></li>
+					</ul>
+				</div>
+				<div class="col-md-4 ftr-grid">
+					<div class="logo-fo">
+						<div class="logo-w3l">
+							<i class="fa fa-stethoscope" aria-hidden="true"></i>
+						</div>
+						<h2><a href="index.php">Human Beat<span>Care For you</span></a></h2>
+					</div>
+				</div>
+				<div class="col-md-4 ftr-grid fg2">
+					<h3>Our Address</h3>
+					<div class="ftr-address">
+						<div class="local">
+							<i class="fa fa-map-marker" aria-hidden="true"></i>
+						</div>
+						<div class="ftr-text">
+							<p>Dhaka,Bangladesh</p>
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+					<div class="ftr-address">
+						<div class="local">
+							<i class="fa fa-phone" aria-hidden="true"></i>
+						</div>
+						<div class="ftr-text">
+							<p>+8801740284027</p>
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+					<div class="ftr-address">
+						<div class="local">
+							<i class="fa fa-envelope" aria-hidden="true"></i>
+						</div>
+						<div class="ftr-text">
+							<p><a href="info@humanbeat.com">info@humanbeat.com</a></p>
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+				<!--<div class="right-w3l">
+					<ul class="top-links">
+						<li class="get-w3"><a href="mail.html"><i class="fa fa-thumbs-o-up" aria-hidden="true"></i>Get In Touch</a></li>
+						<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+						<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+						<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+					</ul>
+				</div>-->
+
+		</div>
+	</div>
+</div>
+<footer class="footer">
+<div class="container-fluid">
+
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <style>
+        /* Set the size of the div element that contains the map */
+        #map {
+          height: 150px;  /* The height is 400 pixels */
+          width: 100%;  /* The width is the width of the web page */
+         }
+      </style>
+    </head>
+    <body>
+      <h3>Here to find us</h3>
+      <!--The div element for the map -->
+      <div id="map"></div>
+      <script>
+  // Initialize and add the map
+  function initMap() {
+    // The location of Uluru
+    var nsu = {lat: 23.8151, lng: 90.4255};
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 18, center: nsu});
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({position: nsu, map: map});
+  }
+      </script>
+      <script async defer
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDDLbp9ciApf2bMJNuRT_8Ki3snPSXRmME&callback=initMap">
+      </script>
+    </body>
+  </html>
+
+</footer>
---
+++ inc/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>HUMAN BEAT | Home</title>
+
+<!-- Meta tag Keywords -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Therapeutic web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!--// Meta tag Keywords -->
+
+<!-- css files -->
+<link rel="stylesheet" href="css/bootstrap.css"> <!-- Bootstrap-Core-CSS -->
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all" /> <!-- Style-CSS -->
+<link rel="stylesheet" href="css/font-awesome.css"> <!-- Font-Awesome-Icons-CSS -->
+<link rel="stylesheet" href="css/jquery-ui.css" /> <!-- Date-script-CSS -->
+<link href="css/wickedpicker.css" rel="stylesheet" type='text/css' media="all" /> <!-- Time-script-CSS -->
+<!-- //css files -->
+
+<!-- online-fonts -->
+<link href="//fonts.googleapis.com/css?family=Convergence" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=ABeeZee:400,400i" rel="stylesheet">
+<!-- //online-fonts -->
+<!--Start of Zendesk Chat Script-->
+<script type="text/javascript">
+window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+_.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
+$.src="https://v2.zopim.com/?5pWPe5M9slFevbCGSxEAARQWj5wL1bXc";z.t=+new Date;$.
+type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
+</script>
+<!--End of Zendesk Chat Script-->
+</head>
