9c86308d492851198549afce88b9bb9e2e14cf2e
MD. JAHIDUL HOSSAIN <jahidcsensu@gmail.com>
2018-12-16 01:23:47 +0600

    Patient Registration and Doctor INformation

--- mail.php
+++ mail.php
@@ -1,13 +1,7 @@
-<!--
-	Author: W3layouts
-	Author URL: http://w3layouts.com
-	License: Creative Commons Attribution 3.0 Unported
-	License URL: http://creativecommons.org/licenses/by/3.0/
--->
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Therapeutic a Medical Category Flat Bootstrap Responsive website Template | Mail :: w3layouts</title>
+<title>HUMAN BEAT || Mail Us</title>
 
 <!-- Meta tag Keywords -->
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -19,7 +13,7 @@ function hideURLbar(){ window.scrollTo(0,1); } </script>
 
 <!-- css files -->
 <link rel="stylesheet" href="css/bootstrap.css"> <!-- Bootstrap-Core-CSS -->
-<link rel="stylesheet" href="css/style.css" type="text/css" media="all" /> <!-- Style-CSS --> 
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all" /> <!-- Style-CSS -->
 <link rel="stylesheet" href="css/font-awesome.css"> <!-- Font-Awesome-Icons-CSS -->
 <link type="text/css" rel="stylesheet" href="css/cm-overlay.css" />
 <!-- //css files -->
@@ -29,7 +23,7 @@ function hideURLbar(){ window.scrollTo(0,1); } </script>
 <link href="//fonts.googleapis.com/css?family=ABeeZee:400,400i" rel="stylesheet">
 <!-- //online-fonts -->
 </head>
-<body> 
+<body>
 <div class="Main-agile">
 <!-- banner -->
 	<div class="banner-2">
@@ -50,16 +44,23 @@ function hideURLbar(){ window.scrollTo(0,1); } </script>
 					<div class="collapse navbar-collapse nav-wil" id="bs-example-navbar-collapse-1">
 						<nav class="stroke">
 							<ul class="nav navbar-nav">
-								<li><a href="index.html">Home</a></li>
-								<li><a href="about.html">About Us</a></li>
+								<li><a href="index.php">Home</a></li>
+								<li><a href="about.php">About Us</a></li>
 								<li class="dropdown menu__item menu__item--current">
-									<a href="#" class="dropdown-toggle menu__link"  data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Pages<span class="caret"></span></a>
-									<ul class="dropdown-menu">
-										<li><a href="codes.html">Short Codes</a></li>
-										<li><a href="icons.html">Icons</a></li>
-									</ul>
-								</li>
-								<li><a href="gallery.html">Gallery</a></li>
+									<a href="#" class="dropdown-toggle menu__link"  data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Patient<span class="caret"></span></a>
+								<ul class="dropdown-menu">
+									<li><a href="emergency_form.php">Emergency Admit</a></li>
+									<li><a href="patient_registration.php">Patient Registration</a></li>
+									<li><a href="patient_login.php">Patient Login</a></li>
+								</ul>
+							</li>
+							<li class="dropdown menu__item menu__item--current">
+								<a href="#" class="dropdown-toggle menu__link"  data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Doctors<span class="caret"></span></a>
+								<ul class="dropdown-menu">
+									<li><a href="doctors_login.php">Doctors Login</a></li>
+									<li><a href="doctors_information.php">Doctors Information</a></li>
+								</ul>
+							</li>
 								<li class="active"><a href="mail.html">Mail Us</a></li>
 							</ul>
 						</nav>
@@ -71,14 +72,14 @@ function hideURLbar(){ window.scrollTo(0,1); } </script>
 				<div class="logo-w3l">
 					<i class="fa fa-stethoscope" aria-hidden="true"></i>
 				</div>
-				<h1><a href="index.html">Therapeutic<span>Care For you</span></a></h1>
+				<h1><a href="index.php">Human Beat<span>Care For you</span></a></h1>
 			</div>
 			<div class="col-md-4 ph-agile">
-				<p><i class="fa fa-phone" aria-hidden="true"></i><span>+00 28 28 28 85</span></p>
+				<p><i class="fa fa-phone" aria-hidden="true"></i><span>+8801740284027</span></p>
 			</div>
 			<div class="clearfix"> </div>
 		</div>
-	</div>			
+	</div>
 </div>
 <!-- //banner -->
 
@@ -89,8 +90,15 @@ function hideURLbar(){ window.scrollTo(0,1); } </script>
 			<li class="item1"><a href="#">Interactive Route Map<span class="glyphicon glyphicon-menu-down"></span></a>
 				<ul>
 					<li class="subitem1">
-						<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3023.9503398796587!2d-73.9940307!3d40.719109700000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25a27e2f24131%3A0x64ffc98d24069f02!2sCANADA!5e0!3m2!1sen!2sin!4v1441710758555"></iframe>
-					</li>										
+						<!--<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3023.9503398796587!2d-73.9940307!3d40.719109700000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25a27e2f24131%3A0x64ffc98d24069f02!2sCANADA!5e0!3m2!1sen!2sin!4v1441710758555"></iframe>
+AIzaSyAvybibU7s23-D58Z4_4basp0_C24KIHXc			-->
+			<iframe
+  width="600"
+  height="450"
+  frameborder="0" style="border:0"
+  src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJk4A6EEzGVTcRSillA_WkYNY&key=AIzaSyAvybibU7s23-D58Z4_4basp0_C24KIHXc" allowfullscreen>
+</iframe>
+				</li>
 				</ul>
 			</li>
 		</ul>
@@ -103,12 +111,12 @@ function hideURLbar(){ window.scrollTo(0,1); } </script>
 			<h3 class="mail-w3">Mail Us<span></span></h3>
 			<div class="w3agile_mail_grids">
 				<div class=" w3agile_mail_grid">
-					<form action="#" method="post">
+					<form action="mail_us_connect.php" method="post">
 						<input type="text" placeholder="Name" name="Name" required="">
 						<input type="email" placeholder="Email" name="Email" required="">
 						<textarea name="Message" placeholder="message" required=""></textarea>
 						<div class="con-form text-center">
-							<input type="submit" value="Send">
+							<input type="submit" name="mail_send" value="Send">
 						</div>
 					</form>
 
@@ -127,10 +135,10 @@ function hideURLbar(){ window.scrollTo(0,1); } </script>
 				<div class="col-md-4 services-grids">
 					<h3>About Us</h3>
 					<ul>
-						<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><a href="single.html">Lorem ipsum dolor sit amet, consectetur.</a></li>
-						<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><a href="single.html">do eiusmod tempor incididunt ut labore.</a></li>
-						<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><a href="single.html">et dolore magna aliqua.</a></li>
-						<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><a href="single.html">onec dut quam lscel quam erisque.</a></li>
+						<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><a href="single.html">Centre of Excellence</a></li>
+						<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><a href="single.html">Excellence in Surgeries</a></li>
+						<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><a href="single.html">Pioneer in Emergency Handling</a></li>
+						<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><a href="single.html">Future Plans</a></li>
 					</ul>
 				</div>
 				<div class="col-md-4 ftr-grid">
@@ -138,7 +146,7 @@ function hideURLbar(){ window.scrollTo(0,1); } </script>
 						<div class="logo-w3l">
 							<i class="fa fa-stethoscope" aria-hidden="true"></i>
 						</div>
-						<h2><a href="index.html">Therapeutic<span>Care For you</span></a></h2>
+						<h2><a href="index.php">Human Beat<span>Care For you</span></a></h2>
 					</div>
 				</div>
 				<div class="col-md-4 ftr-grid fg2">
@@ -148,7 +156,7 @@ function hideURLbar(){ window.scrollTo(0,1); } </script>
 							<i class="fa fa-map-marker" aria-hidden="true"></i>
 						</div>
 						<div class="ftr-text">
-							<p>Lorem ipsum dolor sit amet.</p>
+							<p>Dhaka,Bangladesh</p>
 						</div>
 						<div class="clearfix"> </div>
 					</div>
@@ -157,7 +165,7 @@ function hideURLbar(){ window.scrollTo(0,1); } </script>
 							<i class="fa fa-phone" aria-hidden="true"></i>
 						</div>
 						<div class="ftr-text">
-							<p>+1 (512) 154 8176</p>
+							<p>+8801740284027</p>
 						</div>
 						<div class="clearfix"> </div>
 					</div>
@@ -166,36 +174,25 @@ function hideURLbar(){ window.scrollTo(0,1); } </script>
 							<i class="fa fa-envelope" aria-hidden="true"></i>
 						</div>
 						<div class="ftr-text">
-							<p><a href="mailto:info@example.com">info@example.com</a></p>
+							<p><a href="mailto:info@humanbeat.com">info@humanbeat.com</a></p>
 						</div>
 						<div class="clearfix"> </div>
 					</div>
 				</div>
 				<div class="clearfix"> </div>
 			</div>
-				<div class="right-w3l">
-					<ul class="top-links">
-						<li class="get-w3"><a href="mail.html"><i class="fa fa-thumbs-o-up" aria-hidden="true"></i>Get In Touch</a></li>
-						<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-						<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-						<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-					</ul>
-				</div>
-			<div class="copyrights">
-				<p>&copy; 2017 Therapeutic. All Rights Reserved | Design by  <a href="http://w3layouts.com/" target="_blank">W3layouts</a> </p>
-			</div>
 		</div>
 	</div>
 </div>
 <!-- //Footer -->
 
-<!-- js-scripts -->			
-	
+<!-- js-scripts -->
+
 		<!-- js -->
 			<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
-			<script type="text/javascript" src="js/bootstrap.js"></script> <!-- Necessary-JavaScript-File-For-Bootstrap --> 
+			<script type="text/javascript" src="js/bootstrap.js"></script> <!-- Necessary-JavaScript-File-For-Bootstrap -->
 		<!-- //js -->
-		
+
 		<!-- jarallax-js -->
 			<script src="js/jarallax.js"></script>
 			<script src="js/SmoothScroll.min.js"></script>
@@ -208,17 +205,17 @@ function hideURLbar(){ window.scrollTo(0,1); } </script>
 				})
 			</script>
 		<!-- //jarallax-js -->
-		
+
 		<!-- for-Map -->
 			<!-- script for tabs -->
 		<script type="text/javascript">
 			$(function() {
-			
+
 				var menu_ul = $('.faq > li > ul'),
 					   menu_a  = $('.faq > li > a');
-				
+
 				menu_ul.hide();
-			
+
 				menu_a.click(function(e) {
 					e.preventDefault();
 					if(!$(this).hasClass('active')) {
@@ -230,12 +227,12 @@ function hideURLbar(){ window.scrollTo(0,1); } </script>
 						$(this).next().stop(true,true).slideUp('normal');
 					}
 				});
-			
+
 			});
 		</script>
 		<!-- script for tabs -->
 		<!-- //for-Map -->
-		
+
 <!-- //js-scripts -->
 </body>
 </html>
